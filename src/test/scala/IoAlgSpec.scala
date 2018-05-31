b28ddba795a6c2b2697220649a26d11f1f7606e6
Nicolò Martini <nicolo.martini@springernature.com>
2018-05-31 17:03:02 +0100

    wip

--- src/test/scala/IoAlgSpec.scala
+++ src/test/scala/IoAlgSpec.scala
@@ -1,4 +1,4 @@
-import cats.effect.IO
+import cats.effect.{Fiber, IO}
 import org.scalatest.{Matchers, WordSpec}
 import cats.syntax.all._
 import cats.instances.all._
@@ -19,8 +19,10 @@ class IoAlgSpec extends WordSpec with Matchers {
           else false
         }
       }.toList
+      val n: IO[Unit] = IO { println("hi") } *> IO { println("bye")}
       val shifted: List[IO[Boolean]] = ios.map(IO.shift *> _)
-      //shifted.parSequence.unsafeRunSync()
+      val t: IO[List[Fiber[IO, Boolean]]] = shifted.traverse(_.start)
+      ios.parSequence.unsafeRunSync()
       //shifted.parTraverse()
       val seq = CatsIoAlg.seq(shifted, identity[Boolean], IO.pure(false))
       //val result = seq.unsafeRunSync()
@@ -32,4 +34,46 @@ class IoAlgSpec extends WordSpec with Matchers {
     //io1.
     ???
   }
+
+  // Start writing your ScalaFiddle code here
+  import cats._, implicits._
+  import cats.effect._
+  import scala.concurrent.duration._
+
+  val tasks = List.range(1, 10).map(i =>
+    IO.sleep(i.seconds) *> IO { println(s"$i") } *> IO.pure(i)
+  )
+
+  def findInParallel[A](fas: List[IO[A]], p: A ⇒ Boolean, z: A): IO[A] =
+    fas.traverse(_.start).flatMap { fibers =>
+      fibers.foldLeft(IO.pure(z)) { (result, fiber) =>
+        result.map(p).ifM(
+          result,
+          fiber.join
+        )
+      }
+    }
+
+  def fastestInParallel[A](fas: List[IO[A]], p: A ⇒ Boolean, z: A): IO[A] =
+    fas match {
+      case Nil => IO.pure(z)
+      case head :: tail => IO.racePair(head, fastestInParallel(tail, p, z)).flatMap {
+          case Left((a, fiberB)) => if (p(a)) IO.pure(a) else fiberB.join
+          case Right((fiberA, b)) => if (p(b)) IO.pure(b) else fiberA.join
+        }
+    }
+
+  def findInParallelC[A](fas: List[IO[A]], p: A ⇒ Boolean, z: A): IO[A] =
+    fas.traverse(_.start).flatMap { fibers =>
+      fibers.foldLeft(IO.pure(z)) { (result, fiber) =>
+        result.map(p).ifM(
+          fiber.cancel *> result,
+          fiber.join
+        )
+      }
+    }
+
+  findInParallel[Int](tasks, _ > 5, 0)
+    .flatMap(i => IO(println(s"found $i")))
+    .unsafeRunAsync(_ => ())
 }
